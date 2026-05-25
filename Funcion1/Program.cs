4bfbb7ce57b2aa367841daf357bd24aa8d319fb1
Ulises <ufrixione@uamv.edu.ni>
2026-05-25 12:11:47 -0600

    feat: Configure variables

---
+++ Funcion1/Program.cs
@@ -0,0 +1,8 @@
+﻿//Crear un menu de pciones para el usuario doinde pueda elegeri 
+// la siguientes opciones
+//1. sumar dos numeros
+//2. restar dos numeros
+//3. multiplicar dos numeros
+//4. dividir dos numeros
+//5. Salir
+int num1, num2, num3
