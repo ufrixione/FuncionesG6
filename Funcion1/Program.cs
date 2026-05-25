bb3a7687f1d0a57aa581de7a17e0faaca7cf26a9
Ezequiel <cereyesr@uamv.edu.ni>
2026-05-25 12:46:47 -0600

    feat: add menu

--- Funcion1/Program.cs
+++ Funcion1/Program.cs
@@ -5,4 +5,17 @@
 //3. multiplicar dos numeros
 //4. dividir dos numeros
 //5. Salir
-int num1, num2, num3;
+int num1, num2, resultado, opcion;
+
+
+int menu(int opcion);
+{
+    Console.WriteLine("Seleccione una opcion:");
+    Console.WriteLine("1. Sumar dos numeros");
+    Console.WriteLine("2. Restar dos numeros");
+    Console.WriteLine("3. Multiplicar dos numeros");
+    Console.WriteLine("4. Dividir dos numeros");
+    Console.WriteLine("5. Salir");
+    opcion = Convert.ToInt32(Console.ReadLine());
+    return opcion;
+}
