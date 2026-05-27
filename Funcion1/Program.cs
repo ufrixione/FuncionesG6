bb642f5de4839c742d71d1e4f38a6aa3a36b5ec8
jljuarez06 <jljuarez@uam.edu.ni>
2026-05-27 12:27:36 -0600

    actualizacion del proyecto

--- Funcion1/Program.cs
+++ Funcion1/Program.cs
@@ -6,3 +6,8 @@
 //4. dividir dos numeros
 //5. Salir
 int num1, num2, resultado, opcion;
+
+int multiplicacion (int num1, int num2)
+{
+    return num1 * num2;
+}
--- Funcion1/obj/Debug/net10.0/Funcion1.AssemblyInfo.cs
+++ Funcion1/obj/Debug/net10.0/Funcion1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Funcion1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de11a4afe0296c483aa2d86c909b64fdb58c7f5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9643fe7ee95af5c584941c5b143454501dbfafe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Funcion1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Funcion1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
